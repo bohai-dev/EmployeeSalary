9b9db52f0e6dd15dcbf48d5a3c2ed99c34b03916
cxy <cxy@DESKTOP-P8KOVCD>
2017-08-18 10:16:24 +0800

    123

--- src/main/java/com/bohai/employeeSalary/test/Test.java
+++ src/main/java/com/bohai/employeeSalary/test/Test.java
@@ -1,13 +1,24 @@
 package com.bohai.employeeSalary.test;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Test {
 	
 	
 	
 	public static void main(String[] args) {
-		String time="2017年05月";
+		/*String time="2017年05月";
 		String str=time.replace("年", "-").replace("月", "");
-		System.out.println(str);
+		System.out.println(str);*/
+		/*LocalDate localDate = LocalDate.now();
+		System.out.println("localDate: " + localDate);*/		
+		
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		String dateString = dateTimeFormatter.format(LocalDateTime.now());
+		System.out.println("日期转字符串: " + dateString);
+
 	}
 
 }

83c526a7911167aa515ef56a0d9112bf3f6e77d9
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2017-08-29 12:39:05 +0800

    压缩文件

--- src/main/java/com/bohai/employeeSalary/util/ZipUtil.java
+++ src/main/java/com/bohai/employeeSalary/util/ZipUtil.java
@@ -23,8 +23,8 @@ public class ZipUtil {
             //ZipOutputStream类：完成文件或文件夹的压缩  
             ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipfile));  
             for (int i = 0; i < srcfile.length; i++) {  
-                FileInputStream in = new FileInputStream(srcfile[i]);  
-                out.putNextEntry(new ZipEntry(srcfile[i].getName()));  
+                FileInputStream in = new FileInputStream(srcfile[i]);
+                out.putNextEntry(new ZipEntry(srcfile[i].getName()));
                 int len;  
                 while ((len = in.read(buf)) > 0) {  
                     out.write(buf, 0, len);  
